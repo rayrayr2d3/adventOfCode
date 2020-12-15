55c5eef52c69506279cb9dc8db225fecca1e97d4
Rachel Inderhees <rachelinderhees@gmail.com>
2020-12-15 08:17:29 -0600

    2020-Day-Two Complete

--- 2020/2 - Two/Solution.cs
+++ 2020/2 - Two/Solution.cs
@@ -11,17 +11,57 @@ namespace Twenty.Two
         {
             var input = GetInput().ToList();
 
+            var count = 0;
+
             for (int i = 0; i < input.Count; i++)
             {
-                Console.WriteLine(input);
+                var vals = input[i].Split(" ");
+                var bounds = vals[0].Split("-");
+                var lowerBound = int.Parse(bounds[0]);
+                var higherBound = int.Parse(bounds[1]);
+                var letter = vals[1].ToCharArray()[0];
+                var password = vals[2].ToCharArray();
+
+                var val = 0;
+
+                for (int j = 0; j < password.Length; j++)
+                    if (letter.Equals(password[j])) val++;
+
+                if (val >= lowerBound && val <= higherBound) count++;
             }
+
+            Console.WriteLine(count);
         }
 
         public static void PartTwo()
         {
             var input = GetInput().ToList();
 
+            var count = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var vals = input[i].Split(" ");
+                var bounds = vals[0].Split("-");
+                var firstPosition = int.Parse(bounds[0]) - 1;
+                var secondPosition = int.Parse(bounds[1]) - 1;
+                var letter = vals[1].ToCharArray()[0];
+                var password = vals[2].ToCharArray();
+
+                if (password[firstPosition] == letter || password[secondPosition] == letter)
+                {
+                    if (password[firstPosition] != letter && password[secondPosition] == letter)
+                        count++;
+
+                    if (password[firstPosition] == letter && password[secondPosition] != letter)
+                        count++;
+                }
+            }
+
+            Console.WriteLine(count);
         }
+
+        // 813 too high
 
         public static IEnumerable<string> GetInput()
         {
