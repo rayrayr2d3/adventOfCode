b6f5cfa9f9f62814d116c5088a86259b3c8a7ac2
Rachel Inderhees <rachelinderhees@gmail.com>
2021-01-15 11:08:51 -0600

    2015 Day Two Part One Solution

--- 2015/02 - Two/Solution.cs
+++ 2015/02 - Two/Solution.cs
@@ -3,18 +3,35 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace Fiteen.Two
+namespace Fifteen.Two
 {
     public class Solution
     {
         public static void PartOne()
         {
             var input = GetInput().ToList();
+            var total = 0;
 
             for (int i = 0; i < input.Count; i++)
             {
-                Console.WriteLine(input);
+                var values = input[i].Split('x');
+                var length = Int32.Parse(values[0]);
+                var width = Int32.Parse(values[1]);
+                var height = Int32.Parse(values[2]);
+
+                var sideOne = length * width;
+                var sideTwo = width * height;
+                var sideThree = height * length;
+
+                var smallestSide = 0;
+                if (sideOne <= sideTwo && sideOne <= sideThree) smallestSide = sideOne;
+                else if (sideTwo <= sideOne && sideTwo <= sideThree) smallestSide = sideTwo;
+                else if (sideThree <= sideOne && sideThree <= sideTwo) smallestSide = sideThree;
+
+                total = total + 2 * sideOne + 2 * sideTwo + 2 * sideThree + smallestSide;
             }
+
+            Console.WriteLine(total);
         }
 
         public static void PartTwo()
--- 2015/Program.cs
+++ 2015/Program.cs
@@ -18,26 +18,26 @@ namespace Fifteen
                     if (part.Equals("1")) One.Solution.PartOne();
                     if (part.Equals("2")) One.Solution.PartTwo();
                     break;
-                //case "2":
-                //    if (part.Equals("1")) Two.Solution.PartOne();
-                //    if (part.Equals("2")) Two.Solution.PartTwo();
-                //    break;
-                //case "3":
-                //    if (part.Equals("1")) Three.Solution.PartOne();
-                //    if (part.Equals("2")) Three.Solution.PartTwo();
-                //    break;
-                //case "15":
-                //    if (part.Equals("1")) Fifteen.Solution.PartOne();
-                //    if (part.Equals("2")) Fifteen.Solution.PartTwo();
-                //    break;
-                //case "16":
-                //    if (part.Equals("1")) Sixteen.Solution.PartOne();
-                //    if (part.Equals("2")) Sixteen.Solution.PartTwo();
-                //    break;
-                //case "17":
-                //    if (part.Equals("1")) Seventeen.Solution.PartOne();
-                //    if (part.Equals("2")) Seventeen.Solution.PartTwo();
-                //    break;
+                case "2":
+                    if (part.Equals("1")) Two.Solution.PartOne();
+                    if (part.Equals("2")) Two.Solution.PartTwo();
+                    break;
+                    //case "3":
+                    //    if (part.Equals("1")) Three.Solution.PartOne();
+                    //    if (part.Equals("2")) Three.Solution.PartTwo();
+                    //    break;
+                    //case "15":
+                    //    if (part.Equals("1")) Fifteen.Solution.PartOne();
+                    //    if (part.Equals("2")) Fifteen.Solution.PartTwo();
+                    //    break;
+                    //case "16":
+                    //    if (part.Equals("1")) Sixteen.Solution.PartOne();
+                    //    if (part.Equals("2")) Sixteen.Solution.PartTwo();
+                    //    break;
+                    //case "17":
+                    //    if (part.Equals("1")) Seventeen.Solution.PartOne();
+                    //    if (part.Equals("2")) Seventeen.Solution.PartTwo();
+                    //    break;
             }
         }
     }
