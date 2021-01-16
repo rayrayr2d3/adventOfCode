f5c8771a1c4a36da94f956cf5851a1644821a80a
Rachel Inderhees <rachelinderhees@gmail.com>
2021-01-16 16:16:08 -0600

    2016 Day One Part One Solution

--- 2016/01 - One/Solution.cs
+++ 2016/01 - One/Solution.cs
@@ -9,12 +9,75 @@ namespace Sixteen.One
     {
         public static void PartOne()
         {
-            var input = GetInput().ToList();
+            var input = GetInput().ToList()[0].Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            var x = 0;
+            var y = 0;
 
-            for (int i = 0; i < input.Count; i++)
+            var direction = "";
+
+            for (int i = 0; i < input.Length; i++)
             {
-                Console.WriteLine(input);
+                int distance = Int32.Parse(input[i].Trim().Substring(1));
+
+                if (string.IsNullOrWhiteSpace(direction) || direction.Equals("N"))
+                {
+                    if (input[i][0].Equals('R'))
+                    {
+                        direction = "E";
+                        x += distance;
+                    }
+
+                    if (input[i][0].Equals('L'))
+                    {
+                        direction = "W";
+                        x -= distance;
+                    }
+                }
+                else if (direction.Equals("S"))
+                {
+                    if (input[i][0].Equals('R'))
+                    {
+                        direction = "W";
+                        x -= distance;
+                    }
+
+                    if (input[i][0].Equals('L'))
+                    {
+                        direction = "E";
+                        x += distance;
+                    }
+                }
+                else if (direction.Equals("E"))
+                {
+                    if (input[i][0].Equals('R'))
+                    {
+                        direction = "S";
+                        y -= distance;
+                    }
+
+                    if (input[i][0].Equals('L'))
+                    {
+                        direction = "N";
+                        y += distance;
+                    }
+                }
+                else if (direction.Equals("W"))
+                {
+                    if (input[i][0].Equals('R'))
+                    {
+                        direction = "N";
+                        y += distance;
+                    }
+
+                    if (input[i][0].Equals('L'))
+                    {
+                        direction = "S";
+                        y -= distance;
+                    }
+                }
             }
+
+            Console.WriteLine($"X: {x}, Y: {y}, Total: {x + y}");
         }
 
         public static void PartTwo()
