90f1117f0c73093f5a61f96ee43738c1f2ca7da9
Rachel Inderhees <rachelinderhees@gmail.com>
2021-01-25 13:29:25 -0600

    2018 Day One Part One Solution

--- 2018/Program.cs
+++ 2018/Program.cs
@@ -7,6 +7,38 @@ namespace Eighteen
         static void Main(string[] args)
         {
             Console.WriteLine("Solutions to Advent of Code 2018.");
+            Console.WriteLine("What day?: ");
+            var day = Console.ReadLine();
+            Console.WriteLine("Part one (1) or part two (2)?: ");
+            var part = Console.ReadLine();
+
+            switch (day)
+            {
+                case "1":
+                    if (part.Equals("1")) One.Solution.PartOne();
+                    if (part.Equals("2")) One.Solution.PartTwo();
+                    break;
+                case "2":
+                    if (part.Equals("1")) Two.Solution.PartOne();
+                    if (part.Equals("2")) Two.Solution.PartTwo();
+                    break;
+                    //case "3":
+                    //    if (part.Equals("1")) Three.Solution.PartOne();
+                    //    if (part.Equals("2")) Three.Solution.PartTwo();
+                    //    break;
+                    //case "15":
+                    //    if (part.Equals("1")) Fifteen.Solution.PartOne();
+                    //    if (part.Equals("2")) Fifteen.Solution.PartTwo();
+                    //    break;
+                    //case "16":
+                    //    if (part.Equals("1")) Sixteen.Solution.PartOne();
+                    //    if (part.Equals("2")) Sixteen.Solution.PartTwo();
+                    //    break;
+                    //case "17":
+                    //    if (part.Equals("1")) Seventeen.Solution.PartOne();
+                    //    if (part.Equals("2")) Seventeen.Solution.PartTwo();
+                    //    break;
+            }
         }
     }
 }
